608d65881ef0532560d9e17249b2b2f02e5bd9ca
janacm <janac.meena@gmail.com>
2022-02-14 09:43:38 -0500

    wildcards

--- postgresql.sql
+++ postgresql.sql
@@ -1,1 +1,19 @@
-SELECT * 
+SELECT *
+FROM "automated-speed-enforcement";
+
+-- Wildcards using %
+SELECT *
+FROM "automated-speed-enforcement"
+WHERE "Location*" LIKE 'Military%';
+
+-- Multiple Wildcards, using _
+SELECT *
+FROM "automated-speed-enforcement"
+WHERE "Location*" LIKE 'Royalcrest%'
+    OR "Enforcement End Date" LIKE '__-Oct-__';
+
+-- Wildcards using NOT
+SELECT *
+FROM "automated-speed-enforcement"
+WHERE "Location*" NOT LIKE 'Royalcrest%';
+
